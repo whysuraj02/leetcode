5f2a87764180181b61bb7d4fbafb1fdf42c04513
Suraj sahni <surajsahni2443@gmail.com>
2026-09-24 19:10:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.09%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -3,14 +3,14 @@ class Solution:
         if num == 1:
             return True
         l = 1
-        r = num
-        while l <= r:
-            mid = (l + r) // 2
+        h = num // 2
+        while l <= h:
+            mid = (l + h) // 2
             if mid * mid == num:
                 return True
             elif mid * mid < num:
                 l = mid + 1
             else:
-                r = mid - 1
+                h = mid - 1
             
         return False
